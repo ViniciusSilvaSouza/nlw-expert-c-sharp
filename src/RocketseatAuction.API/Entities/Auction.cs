643fac77678d3bcf5efd00b8c10087424a7e7519
vinic <vinic@DESKTOP-AJIG998>
2024-02-06 13:19:30 -0300

    Add project files.

---
+++ src/RocketseatAuction.API/Entities/Auction.cs
@@ -0,0 +1,14 @@
+﻿using System.Security.Cryptography;
+
+namespace RocketseatAuction.API.Entities;
+
+public class Auction
+{
+    public int Id { get; set; }
+    public string Name { get; set; } = string.Empty;
+    public DateTime Ends { get; set; }
+    public DateTime Starts { get; set; }
+    public List<Item> Items { get; set; } = new List<Item>();
+
+
+}
---
+++ src/RocketseatAuction.API/UseCases/Auctions/GetCurrent/GetCurrentAuctionUseCase.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RocketseatAuction.API.Entities;
+using RocketseatAuction.API.Repositories;
+
+namespace RocketseatAuction.API.UseCases.Auctions.GetCurrent;
+
+public class GetCurrentAuctionUseCase
+{
+    public Auction? Excute()
+    {
+        var respository = new RocketseatAuctionDbContext();
+
+        return respository
+            .Auctions
+            .Include(auction => auction.Items)
+            .FirstOrDefault();
+    }
+}
